8bdfa8226ad6106fe16ff8a925dc2960b86fd6b1
Zoltan <zdebre@gmail.com>
2014-09-03 10:55:57 +1200

    Updating router

--- app/router.js
+++ app/router.js
@@ -5,6 +5,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('income');
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,4 @@ import Ember from 'ember';
 import RawDataArray from 'graph-app/models/raw-data-array';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.get('rawData');
-  },
-
-  rawData: RawDataArray
 });
