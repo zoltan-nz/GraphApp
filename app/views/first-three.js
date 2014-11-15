033dc98a322723802222bfd24dc3808f462c6215
Zoltan <zdebre@gmail.com>
2014-11-16 01:11:39 +1300

    The first three view implemented

---
+++ app/views/first-three.js
@@ -0,0 +1,44 @@
+export default Ember.View.extend({
+
+  width: function() {
+    return window.innerWidth
+  }.property(),
+
+  height: function() {
+    return window.innerHeight;
+  }.property(),
+
+  scene: function() {
+    return new THREE.Scene();
+  }.property(),
+
+  camera: function() {
+    return new THREE.PerspectiveCamera(75, this.get('width') / this.get('height'))
+  }.property('width', 'height'),
+
+  geometry: function() {
+    return new THREE.BoxGeometry(200, 200, 200);
+  }.property(),
+
+  material: function() {
+    return new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true });
+  }.property(),
+
+  mesh: function() {
+    return new THREE.Mesh( this.get('geometry'), this.get('material'))
+  }.property('geometry', 'material'),
+
+  renderer: function() {
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( this.get('width'), this.get('height') );
+    return renderer;
+  }.property('width', 'height'),
+
+  didInsertElement: function() {
+    this.get('scene').add(this.get('mesh'));
+    this.$().append(this.get('renderer.domElement'));
+    this.get('renderer').render(this.get('scene'), this.get('camera'));
+  }
+
+
+})
