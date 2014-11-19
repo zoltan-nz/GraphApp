fb7a769b7f5cb82d1374bb152690fba4bb4bed16
Zoltan <zdebre@gmail.com>
2014-11-19 22:04:22 +1300

    Moving everything in didInsert

--- app/views/first-three.js
+++ app/views/first-three.js
@@ -5,41 +5,35 @@ export default Ember.View.extend({
 
   scene: function() {
     return new THREE.Scene();
-  }.property(),
-
-  camera: function() {
-    return new THREE.PerspectiveCamera(150, 1, 0.1, 1000);
-  }.property('width', 'height'),
-
-  geometry: function() {
-    return new THREE.BoxGeometry(1, 1, 1);
-  }.property(),
-
-  material: function() {
-    return new THREE.MeshBasicMaterial( { color: 0xff0000 });
-  }.property(),
-
-  cube: function() {
-    return new THREE.Mesh( this.get('geometry'), this.get('material'));
-  }.property('geometry', 'material'),
-
-  renderer: function() {
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( this.get('width'), this.get('height') );
-    return renderer;
-  }.property('width', 'height'),
-
-  renderAnimation: function() {
-    //requestAnimationFrame (this.get('render'));
-    //this.get('renderer').render(this.get('scene'), this.get('camera'));
   },
 
   didInsertElement: function() {
-    this.$().append(this.get('renderer.domElement'));
-    //this.get('scene').add(this.get('renderer'));
-    //this.get('renderer').render(this.get('scene'), this.get('camera'));
 
-    //this.renderAnimation();
+    var scene = this.get('scene').apply();
+    var camera = new THREE.PerspectiveCamera( 75, this.get('width')/this.get('height'), 0.1, 1000);
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( this.get('width'), this.get('width') );
+
+    this.$().append( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    function render() {
+      requestAnimationFrame( render );
+
+      cube.rotation.x += 0.1;
+      cube.rotation.y += 0.1;
+
+      renderer.render( scene, camera );
+    }
+
+    render();
   }
 
 });
