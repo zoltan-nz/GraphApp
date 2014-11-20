c1f706a7371adbeb0132e368c74d382c439925c5
Zoltan <zdebre@gmail.com>
2014-11-20 22:46:47 +1300

    More element as ember property

--- app/views/first-three.js
+++ app/views/first-three.js
@@ -2,38 +2,72 @@ export default Ember.View.extend({
 
   width: 300,
   height: 300,
+  color: 0x00ff00,
+  z: 5,
 
   scene: function() {
     return new THREE.Scene();
-  },
+  }.property(),
+
+  camera: function() {
+    return new THREE.PerspectiveCamera( 75, this.get('width')/this.get('height'), 0.1, 1000);
+  }.property('width', 'height'),
+
+  renderer: function() {
+    return new THREE.WebGLRenderer();
+  }.property(),
+
+  geometry: function() {
+    return new THREE.BoxGeometry( 1,1,1 );
+  }.property(),
+
+  material: function() {
+    return new THREE.MeshBasicMaterial( { color: this.get('color')});
+  }.property('color'),
+
+  cube: function() {
+    return new THREE.Mesh( this.get('geometry'), this.get('material') );
+  }.property('geometry', 'material'),
+
+  rendererSizeChanged: function() {
+    this.get('renderer').setSize(this.get('width'), this.get('width'));
+  }.on('init').observes('width', 'height'),
+
+  cameraZChanged: function() {
+    this.get('camera').position.z = this.get('z');
+  }.on('init').observes('z'),
 
   didInsertElement: function() {
 
-    var scene = this.get('scene').apply();
-    var camera = new THREE.PerspectiveCamera( 75, this.get('width')/this.get('height'), 0.1, 1000);
+    var scene = this.get('scene');
+    var camera = this.get('camera');
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( this.get('width'), this.get('width') );
+    //var renderer = this.get('renderer');
+    //renderer.setSize( this.get('width'), this.get('width') );
 
-    this.$().append( renderer.domElement );
+    this.$().append( this.get('renderer').domElement );
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    //var geometry = this.get('geometry');
+    //var material = this.get('material');
+    //var cube = new THREE.Mesh( geometry, material );
+    scene.add( this.get('cube') );
 
-    camera.position.z = 5;
+    var that = this;
 
     function render() {
       requestAnimationFrame( render );
 
-      cube.rotation.x += 0.1;
-      cube.rotation.y += 0.1;
+      that.get('cube').rotation.x += 0.1;
+      that.get('cube').rotation.y += 0.1;
 
-      renderer.render( scene, camera );
+      that.get('renderer').render( that.get('scene'), that.get('camera') );
     }
 
     render();
+  },
+
+  click: function() {
+    console.log('click');
   }
 
 });
