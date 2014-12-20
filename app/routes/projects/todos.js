fe5f9115856a5b5f4f041587fdeba73267aea316
Zoltan <zdebre@gmail.com>
2014-12-21 02:42:54 +1300

    Reformat code

--- app/routes/projects/todos.js
+++ app/routes/projects/todos.js
@@ -1,7 +1,11 @@
 export default Ember.Route.extend({
 
-  model: function(params) {
-    return this.store.find('project', params.project_id).then(function(project) {project.get('todos').then(function(todos) {debugger; return todos;})});
+  model: function (params) {
+    return this.store.find('project', params.project_id).then(function (project) {
+      project.get('todos').then(function (todos) {
+        return todos;
+      });
+    });
   }
 
 });
